9a380e9e7817494cc19925195e3d700fe4bff9d1
jordan rhea <rheajt@gmail.com>
2016-10-27 16:34:05 +0300

    refactoring dojo.js

--- js/dojo.js
+++ js/dojo.js
@@ -1,3 +1,103 @@
+var $ = require('jquery');
+
+(function($) {
+
+  function init() {
+    chrome.storage.sync.get(function(items) {
+      if(items.attendance_taker) {
+        $('#email-control').show();
+        $('#attendance').click(composeEmail);
+      }
+      if(items.spreadsheet_data) {
+        $('#spreadsheet-control').show();
+        $('#spreadsheet').click(writeToSheet);
+      }
+    })
+  }
+
+  function composeEmail() {
+    data.types = {
+      late: document.getElementById('send-late').checked,
+      absent: document.getElementById('send-absent').checked
+    };
+
+    chrome.tabs.executeScript({
+      code: 'composeEmail("' + data.attendance_taker + '","' + data.your_email + '","' + data.types.late + '","' + data.types.absent + '")'
+    });
+  }
+
+  function writeToSheet() {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      gapi.load('client', function() {
+
+        gapi.auth.setToken({
+          access_token: token
+        });
+
+        gapi.client.load('https://sheets.googleapis.com/$discovery/rest?version=v4').then(function(e) {
+          var absentStudents, allStudents;
+
+          gapi.client.sheets.spreadsheets.values.get({
+            "spreadsheetId": data.spreadsheet_data,
+            "range": "ATTENDANCE!A2:D1000"
+          }).then(function(response) {
+            roster = response.result.values;
+
+            chrome.tabs.executeScript({
+              code: 'getStudents()'
+            }, function(students) {
+              absentStudents = students[0].absent;
+              lateStudents = students[0].late;
+
+              roster.forEach(function(each) {
+                for(var i = 0; i < absentStudents.length; i++) {
+                  if(absentStudents[i] === each[0]) {
+                    if(isNaN(each[1])) {
+                      each[1] = 1;
+                    } else {
+                      +each[1]++;
+                    }
+                  }
+                }
+
+                for(var j = 0; j < lateStudents.length; j++) {
+                  if(lateStudents[j] === each[0]) {
+                    if(isNaN(each[2])) {
+                      each[2] = 1;
+                    } else {
+                      +each[2]++;
+                    }
+                  }
+                }
+
+              });
+
+              var updateSheet = gapi.client.sheets.spreadsheets.values.update({
+                "spreadsheetId": data.spreadsheet_data,
+                "range": "ATTENDANCE!A2:D1000",
+                "valueInputOption": "USER_ENTERED",
+                "resource": {
+                  "range": "ATTENDANCE!A2:D1000",
+                  "majorDimension": "ROWS",
+                  "values": roster
+                }
+              });
+
+              updateSheet.execute(function(response) {console.log(response);});
+            });
+          });
+
+        });
+      });
+
+    });
+  }
+
+
+  init();
+}($))
+
+/*
 //saved data
 var data;
 
@@ -102,3 +202,4 @@ function writeToSheet() {
 }
 
 document.addEventListener('DOMContentLoaded', loadData);
+*/
